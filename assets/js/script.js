2383d39b13cc3f3093921d24946ceeae32959b07
María Jesús Fuenzalida Serrano <maria.fuenzalida.serrano@gmail.com>
2020-01-24 14:19:37 -0300

    Sexto avance con funciones JS

---
+++ assets/js/script.js
@@ -0,0 +1,9 @@
+// Funciones
+// Tooltip a href de Sección experiencias
+$(document).ready(function(){
+  $('[data-toggle="tooltip"]').tooltip();
+});
+// Popover en boton comenzar
+$(function () {
+  $('[data-toggle="popover"]').popover()
+});
