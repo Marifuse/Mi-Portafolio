469eab2df565590236b6988277f797a67da27885
María Jesús Fuenzalida Serrano <maria.fuenzalida.serrano@gmail.com>
2020-01-25 20:50:45 -0300

    Decimo avance con la función del scroll y algunos arreglos adicionales en el HTML, CSS, JS

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,23 @@
 // Funciones
-// Tooltip a href de Sección experiencias
+// Función scroll
 $(document).ready(function(){
-  $('[data-toggle="tooltip"]').tooltip();
-});
-// Popover en mi nombre
-$(function () {
-  $('[data-toggle="popover"]').popover()
-});
+    $("a").on('click', function(event) {
+      if (this.hash !== "") {
+        event.preventDefault();
+        var hash = this.hash;
+        $('html, body').animate({
+          scrollTop: $(hash).offset().top
+        }, 800, function(){
+          window.location.hash = hash;
+        });
+      }
+    });
+    // Tooltip a href de Sección experiencias
+    $(document).ready(function(){
+      $('[data-toggle="tooltip"]').tooltip();
+    });
+    // Popover en mi nombre
+    $(function () {
+      $('[data-toggle="popover"]').popover()
+    });
+  });
